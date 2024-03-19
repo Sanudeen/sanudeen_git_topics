9dd32c8d6160bfb00cbaedc9a501073324f7e436
Sikkandar Maidheen Sanudeen <Sikkandar.Sanudeen@ATMECS.CORP>
2024-03-19 15:25:55 +0530

    initcommits

---
+++ arraymethod.js
@@ -0,0 +1,60 @@
+//reverse number with array method join shashi conflict
+
+function reversenum(num)
+{
+    return parseFloat(num.toString().split('').reverse().join(''))*Math.sign(num);
+}
+
+console.log("The reversed number for 1234 using funtion : ", reversenum(1234))
+
+
+const word="Type",words="screen"
+
+const secondword="Script";
+
+const concatword = word.concat(secondword);
+
+console.log(concatword);
+
+//array meethods at(), push(), pop()
+
+const hobbies = ["swimming", "bike riding","painting","cooking"]
+
+let hobbyat = hobbies.at(4);
+console.log("At function : ", hobbyat);
+
+hobbies.push("Music");
+console.log("Push function :", hobbies);
+
+hobbies.pop();
+console.log("Pop function : ", hobbies);
+
+//array methods for search
+
+const vehicles = ["Bike","Car","Lorry","Van","Bike","Bus"]
+
+let indexvalue = vehicles.indexOf("Van");
+console.log("Index value :", indexvalue);
+
+let lastvalue = vehicles.lastIndexOf("Bike");
+console.log("Last index :", lastvalue);
+
+let findvalue = vehicles.includes("Car");
+console.log("Includes Function : ", findvalue);
+
+//array methods sorting
+
+vehicles.sort();
+console.log("Sorting :", vehicles);
+
+//To sort - will create new array and keep the original unchanged
+vehicles.toSorted();
+console.log("To sort function :", vehicles);
+
+vehicles.reverse();
+console.log("Reverse :", vehicles);
+
+//To reverse - will create new array and keep the original unchanged
+
+vehicles.toReversed();
+console.log("To Reverse function :", vehicles);
---
+++ ind.js
@@ -0,0 +1,28 @@
+// for(let i=0;i<10;i++){
+// console.log(i);
+// }
+const check = {adam:32,brian:40,charlie:58,dave:89,envy:12};
+let minval = 40;
+let maxval = 60;
+for (let key in check){
+    console.log(key);
+    if(maxval>=check[key]&&check[key]>=minval ){// added if condition logic
+        console.log(key);
+    }
+}
+
+let fname="Airbnb"
+const check2 = [10,9,8,7,6];
+for (let key in check2){
+    console.log(key,check2[key]);
+    }
+for (let key in check){
+    console.log(key,check[key]);
+       }
+for (let key of check2){
+        console.log(key);
+        }
+console.log("this is 10nd commit")
+for (let key of check2){
+    console.log(key);
+    }
